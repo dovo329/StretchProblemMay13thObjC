0e71ef2bd27a3742d75c312d31f8ae2df0e13488
Doug Voss <douglas.c.voss@gmail.com>
2015-05-13 09:45:42 -0600

    initial commit

---
+++ StretchProblemWedMay13th2015/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StretchProblemWedMay13th2015
+//
+//  Created by Douglas Voss on 5/13/15.
+//  Copyright (c) 2015 DougsApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
